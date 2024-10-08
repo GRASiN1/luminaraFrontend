babc7fbb65df74af0dd1ff53e4a3edd9ab6193bc
GRASiN1 <gauravrajsingh047@gmail.com>
2024-10-08 11:27:00 +0530

    added product route

--- src/services/router.js
+++ src/services/router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/homepage";
+import ProductPage from "../pages/productPage";
 
 // eslint-disable-next-line
 const user = {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/products",
+    element: <ProductPage />,
   },
 ]);
 
