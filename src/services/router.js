a28bc8d5c219de5bed38e0986a3ac547ac5207f2
GRASiN1 <gauravrajsingh047@gmail.com>
2024-10-18 10:32:33 +0530

    added children route in product page

--- src/services/router.js
+++ src/services/router.js
@@ -2,6 +2,8 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/homepage";
 import ProductPage from "../pages/productPage";
+import ProductDetail from "../components/productdetail/productdetail";
+import Products from "../components/products/products";
 
 // eslint-disable-next-line
 const user = {
@@ -16,6 +18,16 @@ const router = createBrowserRouter([
   {
     path: "/products",
     element: <ProductPage />,
+    children: [
+      {
+        index: true,
+        element: <Products />,
+      },
+      {
+        path: "productDetail/:productId",
+        element: <ProductDetail />,
+      },
+    ],
   },
 ]);
 
