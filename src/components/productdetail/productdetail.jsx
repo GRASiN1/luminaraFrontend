d1b69200e232b8b20ccf18a60318c067d3a58450
GRASiN1 <gauravrajsingh047@gmail.com>
2024-10-28 17:01:12 +0530

    updated product detail to use cart context

--- src/components/productdetail/productdetail.jsx
+++ src/components/productdetail/productdetail.jsx
@@ -1,64 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useCart } from "../../contexts/CartContext";
 import { useAlert } from "../../contexts/AlertContext";
 
 export default function ProductDetail() {
   const location = useLocation();
   const { product } = location.state;
-  const [cartItems, setCartItems] = useState([]);
   const { showAlert } = useAlert();
+  const { cartItems, addToCart, removeFromCart } = useCart();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const storedCartItems = localStorage.getItem("cartItems");
-    if (storedCartItems) {
-      setCartItems(JSON.parse(storedCartItems));
-    }
-  }, []);
-
-  function handleBuy(product) {
-    // Retrieve the current cart from localStorage
-    const cart = JSON.parse(localStorage.getItem("cartItems")) || [];
-    // Check if the product already exists in the cart
-    const existingProductIndex = cart.findIndex(
+  function handleBuy() {
+    const existingProductIndex = cartItems.find(
       (item) => item._id === product._id
     );
     if (existingProductIndex !== -1) {
-      // Remove the existing product from the cart
-      cart.splice(existingProductIndex, 1);
-      // Update the cart in localStorage
-      localStorage.setItem("cartItems", JSON.stringify(cart));
+      removeFromCart(product._id);
     }
-    // Save the current product as a new localStorage item with quantity 1
     const newItem = { ...product, quantity: 1 };
     localStorage.setItem("product", JSON.stringify(newItem));
-    // Navigate to checkout
     navigate("/checkout");
   }
 
-  function handleAddToCart(product) {
-    // Find the existing item in the cart by matching the product ID
-    const existingItem = cartItems.find((item) => item._id === product._id);
-
-    if (existingItem) {
-      // If the product already exists, update the quantity
-      const updatedCartItems = cartItems.map((item) =>
-        item._id === product._id
-          ? { ...item, quantity: item.quantity + 1 }
-          : item
-      );
-
-      setCartItems(updatedCartItems);
-      localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
-    } else {
-      // If the product doesn't exist, add it to the cart with a quantity of 1
-      const newItem = { ...product, quantity: 1 };
-      const newCartItems = [...cartItems, newItem];
-
-      setCartItems(newCartItems);
-      localStorage.setItem("cartItems", JSON.stringify(newCartItems));
-    }
-
+  function handleAddToCart() {
+    addToCart(product);
     showAlert("Product added to cart", "Success");
   }
 
@@ -82,13 +47,13 @@ export default function ProductDetail() {
             <p className="text-xl">₹ {product.productPrice}</p>
             <div className="w-full flex flex-row justify-start items-center gap-5 my-3">
               <button
-                onClick={() => handleBuy(product)}
+                onClick={handleBuy}
                 className="bg-black px-3 py-1 text-white rounded-md hover:bg-salmonPink hover:text-black"
               >
                 Buy
               </button>
               <button
-                onClick={() => handleAddToCart(product)}
+                onClick={handleAddToCart}
                 className="bg-black px-3 py-1 text-white rounded-md hover:bg-salmonPink hover:text-black"
               >
                 Add to cart
@@ -97,13 +62,11 @@ export default function ProductDetail() {
           </div>
           <div className="w-full h-full flex flex-col justify-start items-start border-b-1 border-mistyRose pb-3">
             <h3 className="text-xl font-semibold">Product Details</h3>
-            {Object.entries(product.details).map(([key, value]) => {
-              return (
-                <p key={key} className="text-lg">
-                  {key} : {value}
-                </p>
-              );
-            })}
+            {Object.entries(product.details).map(([key, value]) => (
+              <p key={key} className="text-lg">
+                {key} : {value}
+              </p>
+            ))}
           </div>
         </div>
       </div>
